b9b88095f4f0076539e45166fe2d17fcddef6181
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-07-19 16:44:57 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div className="my-style">
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+10 123 456789"
+      email="beyonce@beyonce.com"
+    />
+    <Card
+      name="Chuck Norris"
+      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      tel="+1800-RoundKick"
+      email="BillGates.com - My secretary"
+    />
+  </div>,
+  document.getElementById("root")
+);
